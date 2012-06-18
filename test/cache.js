582501b485abf304337da2655be67f3ac9145beb
Bubi Shimi <bubishimi@gmail.com>
2012-06-19 00:11:15 +0300

    Move to vert.x

--- test/cache.js
+++
@@ -1,9 +0,0 @@
-var Hash = require('hashish');
-var assert = require('assert');
-var cache = require('../lib/cache');
-
-exports.map = function () {
-	var c = new cache.createCache();
-	c.set('a','b')
-    assert.deepEqual(c.get('a'), 'b');
-};
