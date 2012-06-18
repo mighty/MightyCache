c900ab2ffaee229087def19194659117d7df1647
Bubi Shimi <bubishimi@gmail.com>
2012-06-19 00:02:04 +0300

    c

--- test/cache.js
+++ test/cache.js
@@ -1,11 +1,9 @@
 var Hash = require('hashish');
 var assert = require('assert');
+var cache = require('../lib/cache');
 
 exports.map = function () {
-    var ref = { a : 1, b : 2 };
-    var items = Hash(ref).map(function (v) { return v + 1 }).items;
-    var hash = Hash.map(ref, function (v) { return v + 1 });
-    assert.deepEqual(ref, { a : 1, b : 2 });
-    assert.deepEqual(items, { a : 2, b : 3 });
-    assert.deepEqual(hash, { a : 2, b : 3 });
+	var c = new cache.createCache();
+	c.set('a','b')
+    assert.deepEqual(c.get('a'), 'b');
 };
